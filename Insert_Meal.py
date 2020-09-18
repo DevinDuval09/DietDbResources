eddcbf5f4d213435ebf6368d4224f5e8857ccc68
Devin Duval <devinduval09@gmail.com>
2020-09-18 16:03:09 -0700

    Insert_Meal.py: added function to insert meal into database

--- Insert_Meal.py
+++ Insert_Meal.py
@@ -15,9 +15,11 @@ root.title("Meals")
 #database connections
 conx = pyodbc.connect("DRIVER={SQL SERVER NATIVE CLIENT 11.0};SERVER=(local);DATABASE=DietDb;Trusted_Connection=yes")
 cursor = conx.cursor()
-#
-
-
+#Create labels
+lTotalCalories = ttk.Label(root)
+lTotalCalories.pack()
+entryMealName = ttk.Entry()
+entryMealName.pack()
 
 #lists for food that is added to meal
 comboboxlist = []
@@ -40,11 +42,28 @@ def CreateWidgets():
 	qtyEntry.pack()
 	entrylist.append(qtyEntry)
 
-bAddWidgets = ttk.Button(root,text='Add Food to Meal',command=CreateWidgets)
-bAddWidgets.pack()
+#insert current meal into database
+def InsertMeal():
+	#get next pk
+	cursor.execute('SELECT TOP 1 MealsKey FROM Meals ORDER BY MealsKey Desc;')
+	lpk = int(cursor.fetchone()[0])
+	pk = str(lpk + 1)
 
-lTotalCalories = ttk.Label(root)
+	#get inputs
+	name = entryMealName.get()
 
+	#write sql statments
+	insertStatements = []
+	insertStatements.append("INSERT INTO Meals(MealsKey,Name) VALUES(" + pk + "," + name + ");")
+	for ind in range(0,len(comboboxlist)):
+		insertStatements.append("INSERT INTO MealsFood(MealsKey,FoodKey,Quantity) VALUES(" + pk + "," + str(comboboxlist[ind].get().split()[0][2:]) + "," + str(entrylist[ind].get())+");")
+	#insert statements into tables
+	for statement in insertStatements:
+		print(statement)
+		# cursor.execute(statement)
+		# conx.commit()
+
+#display meal calorie total on label
 def CalcCalories():
 	calCount = float(0)
 	for ind in range(0,len(comboboxlist)):
@@ -62,8 +81,15 @@ def CalcCalories():
 	lTotalCalories.pack()
 	lTotalCalories.update()
 
+#create buttons
+bAddWidgets = ttk.Button(root,text='Add Food to Meal',command=CreateWidgets)
+bAddWidgets.pack()
+
 bCalculate = ttk.Button(root,text='Calculate Calories',command=CalcCalories)
 bCalculate.pack()
+
+bSubmit = ttk.Button(root,text='Submit Meal',command =InsertMeal)
+bSubmit.pack()
 
 
 #need to devise a way to track widgets to create insert statement
