c01a249542be5b06847f042b5398bc10ac115f04
Devin Duval <devinduval09@gmail.com>
2020-09-22 11:35:20 -0700

    Insert_FoodEaten.py: added list to track checkbuttons and list to track checkvariables

--- Insert_FoodEaten.py
+++ Insert_FoodEaten.py
@@ -17,15 +17,21 @@ root.title("Food Eaten")
 
 #Entry for date / data view
 cal = tkcalendar.Calendar(root,font="Arial 14", selectmode='day')
-cal.grid(column = 0, row = 0,columnspan = 4)
+cal.grid(column = 0, row = 0,columnspan = 4,rowspan=8)
 
 #create widgets for data view/entry
 cmblist = []
 qtylist = []
+checklist = []
+checkvariables = []
+var = tkinter.IntVar()
+var.set(0)
 
-util.FoodWidgets(root, firstcolumn = 0)
-cmblist.append(root.grid_slaves(0,1))
-qtylist.append(root.grid_slaves(1,1))
+util.FoodWidgets(root, firstcolumn = 0,includecheckbox=True,checkvariable=var)
+cmblist.append(root.grid_slaves(0,8))
+qtylist.append(root.grid_slaves(1,8))
+checklist.append(root.grid_slaves(4,8))
+checkvariables.append(var)
 
 lEaten = ttk.Label()
 lPlanned = ttk.Label()
@@ -34,23 +40,31 @@ lCaloriesPlanned = ttk.Label()
 lCaloriesTotal = ttk.Label()
 
 def AddWidgets():
-	util.FoodWidgets(root,firstcolumn=0)
+	x = tkinter.IntVar()
+	x.set(0)
+	util.FoodWidgets(root,firstcolumn=0,includecheckbox=True,checkvariable = x)
+	checkvariables.append(x)
+	checklist.append(root.grid_slaves(4,root.grid_size()[1]))
 	cmblist.append(root.grid_slaves(0,root.grid_size()[1]))
 	qtylist.append(root.grid_slaves(1,root.grid_size()[1]))
 
 
 def CalcCalories():
-	#column order:foodlist, qtyEntry,lblCalories,bCalc
+	#column order:foodlist, qtyEntry,lblCalories,bCalc,checkbox
+	pass
+
+def Submit():
+	#submit all foods listed to database
 	pass
 	
 bAddFoodWidgets = ttk.Button(root,text="Add Entry Row",command=AddWidgets)
 bAddFoodWidgets.grid(row=0,column=5)
 bCalculate = ttk.Button(root,text="Total Entries",command=CalcCalories)
-bCalculate.grid(row=0,column=6)
+bCalculate.grid(row=1,column=5)
 
 
-#sql statements to view food consumed that day
 
-#sql statements to view food scheduled for that day
+
+#submit button to add everything to database
 
 root.mainloop()
