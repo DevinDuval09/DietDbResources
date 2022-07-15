d071d7961dc14c3b578bd9834ee7f51a77464082
Devin Duval <devinduval09@gmail.com>
2022-07-15 12:06:49 -0700

    removed session import from DbUtil and replaced with an engine created with DbUtil.create_test_engine

--- FoodJournal/forms.py
+++ FoodJournal/forms.py
@@ -1,12 +1,15 @@
 from django.db.models.base import Model
 from django.forms import Form, ModelForm, TextInput, Textarea, DateInput, NumberInput, ChoiceField, Select
 from django.contrib.auth.forms import UserCreationForm
-from .DbUtil import FOOD_CHOICES, session, food_eaten
+from .DbUtil import FOOD_CHOICES, food_eaten, DATABASE_NAME, create_test_engine
+from sqlalchemy.orm.session import sessionmaker, Session
 from sqlalchemy import select, insert
 import logging
 from datetime import datetime
+import os
 
 logger = logging.getLogger(__name__)
+engine = create_test_engine()
 class NewUserForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         fields = UserCreationForm.Meta.fields + ("email",)
@@ -25,13 +28,15 @@ class InputFoodEaten(Form):
         labels = {"date": "Date:", "food": "Food:", "qty": "Quantity:", "calories": "Total Calories:"}
     def save(self, request):
         if self.is_valid():
+            session = sessionmaker(engine)
+            logger.info("Post request and data transformation:")
             logger.info(request.POST)
             data = {}
             data["date"] = datetime.strptime(request.POST["date_input"], '%Y-%m-%d')
             data["food"] = request.POST["food_id"]
             data["qty"] = request.POST["qty_input"]
             data["user"] = request.user.id
-            logger.info(f"\n\n{type(data['date'])}")
+            logger.info(data)
             with session.begin() as s:
                 stmt = insert(food_eaten).values(
                                                 date=data["date"],
