fbde315b101927c5ead518e03fada75bece213e4
Devin Duval <devinduval09@gmail.com>
2022-07-19 11:09:41 -0700

    Removed functions that used SqlAlchemy and replaced them with generic django views.

--- FoodJournal/views.py
+++ FoodJournal/views.py
@@ -3,12 +3,15 @@ from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib.auth import login
+from django.views.generic import ListView, CreateView
 from .DbUtil import *
+from .models import Meals, Foods, Measurements, get_food_model
 from sqlalchemy import select, insert
 from sqlalchemy.exc import IntegrityError
 import os
 from .forms import InputFoodEaten, NewUserForm
 import logging
+from datetime import datetime
 
 logger = logging.getLogger(__name__)
 
@@ -23,15 +26,24 @@ def create_user(request, *args, **kwargs):
     else:
         return render(request, "new_user.html", {"form": form})
 
-def add_meal(request, *args, **kwargs):
-    if request.user.id is None:
-        return redirect("/login/")
-    form = InputFoodEaten(initial={"user": request.user.id})
-    user_name = request.user_name
-    if request.method == "POST":
+class MealsView(ListView):
+    template_name = "FoodJournal/meals_list.html"
+    def get_queryset(self):
+        return Meals.objects.filter(user__username=self.kwargs["username"])
+    def post(self, request, *args, **kwargs):
+        logger.info(request.POST)
+        form = InputFoodEaten(
+            {
+                'cdate':    datetime.strptime(request.POST.get("date_input"), '%Y-%m-%d').date(),
+                'qty':      float(request.POST.get("qty_input")),
+                'food':     int(request.POST.get("food_id")),
+                'user':     int(request.user.id)
+            }
+        )
         if form.is_valid():
             form.save()
-    redirect(f"/FoodJournal/{user_name}/")
+        return redirect(reverse("user_meals", args=[self.kwargs["username"]]))
+
 
 def test_page(request, *args, **kwargs):
     body = "Test Page\n\n"
@@ -43,97 +55,32 @@ def test_page(request, *args, **kwargs):
         body += "\n".join([f"\t{tup}" for tup in kwargs.items()])
     return HttpResponse(body, content_type="text/plain")
 
-def generate_food_journal(request, username, template_name="FoodJournal/FoodJournal.html", *args, **kwargs):
-    if request.user.id is None:
-        return redirect("/login/")
-    if request.method == "GET":
-        template_name = "FoodJournal/FoodJournal.html"
-        kwargs["user_id"] = get_id(users, users.c.username, username)
-        with session.begin() as s:
-            query = select(food_eaten.c.date,
-                            food_info.c.description,
-                            food_eaten.c.qty,
-                            food_info.c.calories_unit)\
-                        .filter(food_eaten.c.food == food_info.c.id, food_eaten.c.user == kwargs["user_id"])
-            results = s.execute(query).all()
-            data = []
-            for row in results:
-                data.append([row[0], row[1], row[2], row[2] * row[3]])
-        return render(request, template_name, {"rows": data})
-    elif request.method == "POST":
-        form = InputFoodEaten(request.POST, initial={"user": request.user.id})
-        if form.is_valid():
-            form.save(request)
-        return redirect(reverse("user_index", args=[username]))
-
-def get_food_info_page(request, template_name="FoodJournal/FoodInfo.html", *args, **kwargs):
-    with session.begin() as s:
-        query = select(food_info.c.description, food_info.c.calories_unit, measurements.c.description).filter(food_info.c.measurement_unit == measurements.c.id)
-        results = s.execute(query).all()
-        data = []
-        for row in results:
-            data.append([row[0].title(), row[1], row[2]])
-        measurement_query = select(measurements)
-        measure_results = s.execute(measurement_query).all()
-        measure_rows = []
-        for mrow in measure_results:
-            measure_rows.append([mrow[0], mrow[1]])
-    context = {"rows": data, "m_table": measure_rows}
-    if "errors" in kwargs.keys():
-        context["errors"] = kwargs["errors"]
-    return render(request, template_name, context)
-
-def post_food_info_page(request, template_name="FoodJournal/FoodInfo.html", *args, **kwargs):
-    #these should match the input field names on FoodInfo.html
-    form_fields = ["food_input",
-                    "measurement_input",
-                    "calorie_input",
-                    "protein_input",
-                    "carbs_input",
-                    "total_fat_input",
-                    "sat_fat_input",
-                    "fiber_input"]
-    errors = []
-    validated_data = {}
-    for field in form_fields:
-        try:
-            if request.POST[field].strip() == '' or request.POST[field] is None:
-                raise KeyError
-            validated_data[field] = request.POST[field]
-        except KeyError:
-            errors.append(f"{field} must have a value.")
-            continue
-    if len(errors) > 0:
-        return get_food_info_page(request, template_name, *args, errors=errors)
-    with session.begin() as s:
-        stmt = insert(food_info).values(description=validated_data["food_input"],
-                                        calories_unit=validated_data["calorie_input"],
-                                        protein_unit=validated_data["protein_input"],
-                                        carbs_unit=validated_data["carbs_input"],
-                                        total_fat_unit=validated_data["total_fat_input"],
-                                        sat_fat_unit=validated_data["sat_fat_input"],
-                                        fiber_unit=validated_data["fiber_input"],
-                                        measurement_unit=validated_data["measurement_input"])
-        try:
-            s.execute(stmt)
-            s.commit()
-            request.method = "GET"
-            message = f"{validated_data['food_input']} has been added."
-            return food_info_page(request, template_name, message=message)
-        except IntegrityError as err:
-            errors.append("Database error: " + repr(err))
-            return get_food_info_page(request, template_name, *args, errors=errors)
-
-def food_info_page(request, template_name="FoodJournal/FoodInfo.html", *args, **kwargs):
-    if request.method == "GET":
-        return get_food_info_page(request, template_name, *args, **kwargs)
-    if request.method == "POST":
-        return post_food_info_page(request, template_name, *args, **kwargs)
+class FoodInfo(ListView):
+    template_name = "FoodJournal/FoodInfo.html"
+    model = Foods
+    queryset = Foods.objects.all()
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["m_table"] = Measurements.objects.all()
+        return context
+    def post(self, request, *args, **kwargs):
+        logger.info(request.POST)
+        form = AddFood({
+            "description":      request.POST.get("food_input"),
+            "calories_unit":    request.POST.get("calories_input"),
+            "protein_unit":     request.POST.get("protein_input"),
+            "carbs_unit":       request.POST.get("carbs_input"),
+            "total_fat_unit":   request.POST.get("total_fat_input"),
+            "sat_fat_unit":     request.POST.get("sat_fat_input"),
+            "fiber_unit":       request.POST.get("fiber_input"),
+            "measurement_unit": request.POST.get("measurement_input"),
+        })
 
 def get_food_json(request, food, *args, **kwargs):
+    logger.info(f"Getting json for {food}...")
     if request.method == "GET":
         food_name = clean_input(food)
-        food_data = get_food_dict(food_name)
+        food_data = get_food_model(food_name)
         return JsonResponse(food_data)
 
 
