8306f9a878b714d9e5fb7e81420f53c5f9cfee3f
Devin Duval <devinduval09@gmail.com>
2022-07-26 15:50:39 -0700

    created setup.py file to add csv data to database

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,1 +1,1 @@
-python manage.py migrate FoodJournal && python DbUtil.py
+python manage.py migrate FoodJournal && python setup.py
---
+++ setup.py
@@ -0,0 +1,3 @@
+from FoodJournal.models import csv_to_model, file_path
+if __name__ == "__main__":
+    csv_to_model(file_path)
