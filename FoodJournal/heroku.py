9f1aab0386b81bb54248af89cd84578784b4cba6
Devin Duval <devinduval09@gmail.com>
2022-07-27 14:16:32 -0700

    trying to fix db issues

--- FoodJournal/heroku.py
+++ FoodJournal/heroku.py
@@ -4,9 +4,11 @@ from DietTracker.settings import *
 
 DATABASES = {
     "default": dj_database_url.config(
-        default="sqlite:///" + os.path.join(BASE_DIR, "db.sqlite3")
-    )
+        default='postgres://bmkvaujwsilfcd:d50e5f1ba7c391c0708e21ac5b2747c903eab5fce1d49ba2ac9aa4d8edd20116@ec2-44-208-88-195.compute-1.amazonaws.com:5432/d13dggive4g2mb'
+)
 }
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 DEBUG=False
 TEMPLATE_DEBUG=False
