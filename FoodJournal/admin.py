427d694cc13996f5724d12fbe4b7fb0fd8f724e9
Devin Duval <devinduval09@gmail.com>
2022-07-28 12:36:11 -0700

    added admin views for models

--- FoodJournal/admin.py
+++ FoodJournal/admin.py
@@ -1,5 +1,34 @@
 from django.contrib import admin
 from .models import Measurements, Foods, Meals
-admin.site.register(Measurements)
-admin.site.register(Foods)
-admin.site.register(Meals)
+
+class MeasurementsInline(admin.TabularInline):
+    model = Measurements
+
+class MeasurementsAdmin(admin.ModelAdmin):
+    list_display = ["id", "description"]
+    ordering = ["description"]
+
+class FoodsInline(admin.TabularInline):
+    model = Foods
+
+class FoodsAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "description",
+        "calories_unit",
+        "protein_unit",
+        "carbs_unit",
+        "total_fat_unit",
+        "sat_fat_unit",
+        "measurement_unit"
+        ]
+    ordering = ["description"]
+
+class MealsAdmin(admin.ModelAdmin):
+    list_display = ["food", "qty", "cdate", "user"]
+    ordering = ["user", "cdate"]
+
+
+admin.site.register(Measurements, MeasurementsAdmin)
+admin.site.register(Foods, FoodsAdmin)
+admin.site.register(Meals, MealsAdmin)
