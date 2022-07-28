147f6cf9db9af2ccf0d771ebe69fb53677469ab3
Devin Duval <devinduval09@gmail.com>
2022-07-28 16:15:21 -0700

    removing db uri call

--- FoodJournal/heroku.py
+++ FoodJournal/heroku.py
@@ -2,11 +2,6 @@ import os
 import dj_database_url
 from DietTracker.settings import *
 
-'''DATABASES = {
-    "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL")
-)
-}'''
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
